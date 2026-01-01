783dfb96328ae4b72cc4fb23db23b92b4c74e2ce
learninggit <milindkhorgade07@gmail.com>
2026-01-01 22:50:15 +0530

    Day 1 – Band Name Generator

---
+++ Day1ProjectBandNameGenerator.py
@@ -0,0 +1,12 @@
+# print("welcome to the band name generator")
+# city = input("which city did you grow up in \n")
+# pet = input("what is a name of a pet\n")
+# print("your band name could be : " + city + " " + pet )
+
+
+print("welcome to band name generator")
+city = input("what is the city you grow up in ?")
+print(city)
+pet =  input("what is your pet name ")
+print(pet)
+print("your band name could be " + city +" "+pet)
